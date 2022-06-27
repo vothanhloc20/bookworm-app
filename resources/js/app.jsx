13f0bfe11a86f89a64994d2297471b9342e6064e
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-06-27 17:43:35 +0700

    feat(fe): update root for react@18

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,14 +2,15 @@ import App from "./App.js";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import store from "./redux/store.js";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
     <Provider store={store}>
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </Provider>,
-    document.getElementById("root")
+    </Provider>
 );
