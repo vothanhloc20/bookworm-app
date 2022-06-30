0bad2364d32646f6bfb3f895aac785f2489bbead
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-06-30 09:42:10 +0700

    fix(be): rewrite scope Book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use App\Http\Traits\CheckDiscount;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Book extends Model
 {
     use HasFactory;
+    use CheckDiscount;
 
     public $timestamps = false;
     protected $table = 'book';
@@ -32,16 +35,72 @@ class Book extends Model
         return $this->hasMany(Review::class);
     }
 
-    public function scopeOnSaleBook($query)
+    public function scopeFilterBase($query, $request)
+    {
+        return $query
+            ->when($request->has('filter.category'), function ($query) use ($request) {
+                $list = explode(',', $request->query('filter')['category']);
+                $query->whereIn('category_name', $list);
+            })
+            ->when($request->has('filter.author'), function ($query) use ($request) {
+                $list = explode(',', $request->query('filter')['author']);
+                $query->whereIn('author_name', $list);
+            });
+    }
+
+    public function scopeSortBase($query, $request)
+    {
+        $query
+            ->when($request->has('sort'), function ($query) use ($request) {
+                $sortBy = '';
+                $sortValue = '';
+
+                foreach ($request->get('sort') as $key => $value) {
+                    $sortBy = $key;
+                    $sortValue = $value;
+                }
+
+                $query
+                    ->when($sortBy === 'popularity', function ($query) {
+                        $query
+                            ->PopularBooks();
+                    })
+                    ->when($sortBy === 'price', function ($query) use ($sortValue) {
+                        $query
+                            ->BaseBooks()
+                            ->orderBy('check_discount.final_price', $sortValue);
+                    });
+            })
+            ->when(!$request->has('sort'), function ($query) {
+                $query->onSaleBooks();
+            });
+
+        return $query;
+    }
+
+    public function scopeJoinWithBooks($query)
     {
         $checkDiscount = $this->checkDiscount();
 
-        return $query
-            ->join('author', 'book.author_id', '=', 'author.id')
-            ->select('book.id', 'book.book_title', 'author.author_name', 'book.book_price', 'book.book_cover_photo', 'check_discount.discount_price', 'check_discount.final_price')
+        return $query->join('author', 'book.author_id', '=', 'author.id')
+            ->join('category', 'book.category_id', '=', 'category.id')
+            ->select('book.id',
+                'book.book_title',
+                'author.author_name',
+                'book.book_price',
+                'book.book_cover_photo',
+                'book.book_summary',
+                'check_discount.discount_price',
+                'check_discount.final_price')
             ->joinSub($checkDiscount, 'check_discount', function ($join) {
                 $join->on('book.id', '=', 'check_discount.id');
-            })
+            });
+    }
+
+    public function scopeOnSaleBooks($query)
+    {
+        return $query
+            ->JoinWithBooks()
             ->where('check_discount.discount_price', '!=', null)
             ->whereRaw('check_discount.discount_price = check_discount.final_price')
             ->selectRaw('book.book_price - check_discount.discount_price AS sub_price')
@@ -49,41 +108,19 @@ class Book extends Model
             ->orderBy('check_discount.final_price', 'asc');
     }
 
-    public function scopeBaseBook($query)
+    public function scopeBaseBooks($query)
     {
-        $checkDiscount = $this->checkDiscount();
-
         return $query
-            ->join('review', 'book.id', '=', 'review.book_id')
-            ->join('author', 'book.author_id', '=', 'author.id')
-            ->select('book.id', 'book.book_title', 'author.author_name', 'book.book_price', 'book.book_cover_photo', 'check_discount.discount_price', 'check_discount.final_price')
-            ->joinSub($checkDiscount, 'check_discount', function ($join) {
-                $join->on('book.id', '=', 'check_discount.id');
-            });
+            ->JoinWithBooks()
+            ->join('review', 'book.id', '=', 'review.book_id');
     }
 
-    public function checkDiscount()
+    public function scopePopularBooks($query)
     {
-        $currentDate = date('Y-m-d');
-
-        return Book::query()
-            ->leftJoin('discount', 'book.id', '=', 'discount.book_id')
-            ->select('book.id', 'discount.discount_price')
-            ->selectRaw("
-                CASE
-                    WHEN discount.discount_price IS NULL
-                    THEN book.book_price
-
-                    WHEN discount.discount_price IS NOT NULL
-                    AND discount.discount_end_date < ?
-                    OR discount.discount_start_date > ?
-                    THEN book.book_price
-
-                    WHEN discount.discount_price IS NOT NULL
-                    AND discount.discount_start_date <= ?
-                    THEN discount.discount_price
-
-                    END
-                AS final_price", [$currentDate, $currentDate, $currentDate]);
+        $query->BaseBooks()
+            ->withCount('review')
+            ->distinct()
+            ->orderByDesc('review_count')
+            ->orderBy('check_discount.final_price', 'asc');
     }
 }
