c185056e9d38a8a7555668b64b76a660ceef936d
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-07-02 02:40:49 +0700

    fix(be): REST Api for Book

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,39 +3,92 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\Book\BookRepository;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    public $bookRepository;
+    public BookRepository $bookRepository;
 
     public function __construct(BookRepository $bookRepository)
     {
         $this->bookRepository = $bookRepository;
     }
 
-    public function getAllBooks(Request $request)
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
     {
-        return $this->bookRepository->getAllBooks($request);
+        return $this->bookRepository->getRecords($request);
     }
 
-    public function getTopTenOnSaleBooks()
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
     {
-        return $this->bookRepository->getTopTenOnSaleBooks();
+        //
     }
 
-    public function getRecommendedBooks()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
     {
-        return $this->bookRepository->getRecommendedBooks();
+        //
     }
 
-    public function getPopularBooks()
-    {
-        return $this->bookRepository->getPopularBooks();
-    }
-
-    public function getBookById($id)
+    /**
+     * Display the specified resource.
+     *
+     * @param int $id
+     * @return JsonResponse|void
+     */
+    public function show(int $id)
     {
         return $this->bookRepository->getBookById($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
-use App\Http\Traits\Book\ScopeBook;
+use App\Http\Traits\CheckDiscount;
+use App\Http\Traits\Review\AverageRatingStar;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
     use HasFactory;
-    use ScopeBook;
+    use CheckDiscount;
+    use AverageRatingStar;
 
     public $timestamps = false;
     protected $table = 'book';
@@ -32,5 +34,115 @@ class Book extends Model
     public function Review()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeJoinWithBooks($query)
+    {
+        $checkDiscount = $this->checkDiscount();
+
+        return $query->join('author', 'book.author_id', '=', 'author.id')
+            ->join('category', 'book.category_id', '=', 'category.id')
+            ->select('book.id',
+                'book.book_title',
+                'author.author_name',
+                'book.book_price',
+                'book.book_cover_photo',
+                'book.book_summary',
+                'check_discount.discount_price',
+                'check_discount.final_price')
+            ->joinSub($checkDiscount, 'check_discount', function ($join) {
+                $join->on('book.id', '=', 'check_discount.id');
+            });
+    }
+
+    public function scopeOnSaleBooks($query)
+    {
+        return $query
+            ->JoinWithBooks()
+            ->where('check_discount.discount_price', '!=', null)
+            ->whereRaw('check_discount.discount_price = check_discount.final_price')
+            ->selectRaw('book.book_price - check_discount.discount_price AS sub_price')
+            ->orderBy('check_discount.final_price', 'asc');
+    }
+
+    public function scopeOnRecommendedBooks($query)
+    {
+        return $query
+            ->JoinWithBooks()
+            ->join('review', 'book.id', '=', 'review.book_id')
+            ->withAvg('review', 'rating_star')
+            ->distinct()
+            ->orderBy('check_discount.final_price', 'asc');
+    }
+
+    public function scopeOnPopularBooks($query)
+    {
+        return $query
+            ->JoinWithBooks()
+            ->join('review', 'book.id', '=', 'review.book_id')
+            ->distinct()
+            ->withCount('review')
+            ->orderBy('check_discount.final_price', 'asc');
+    }
+
+    public function scopeSortBase($query, $request)
+    {
+        return $query
+            ->when($request->has('sort'), function ($query) use ($request) {
+                $sortBy = '';
+                $sortValue = '';
+
+                foreach ($request->get('sort') as $key => $value) {
+                    $sortBy = $key;
+                    $sortValue = $value;
+                }
+
+                $query
+                    ->when($sortBy === 'on_sale', function ($query) use ($sortValue) {
+                        $query
+                            ->onSaleBooks()
+                            ->orderBy('sub_price', $sortValue);
+                    })
+                    ->when($sortBy === 'on_popularity', function ($query) use ($sortValue) {
+                        $query
+                            ->OnPopularBooks()
+                            ->orderBy('review_count', $sortValue);
+                    })
+                    ->when($sortBy === 'on_recommended', function ($query) use ($sortValue) {
+                        $query
+                            ->OnRecommendedBooks()
+                            ->orderBy('review_avg_rating_star', $sortValue);
+                    })
+                    ->when($sortBy === 'price', function ($query) use ($sortValue) {
+                        $query
+                            ->JoinWithBooks()
+                            ->orderBy('check_discount.final_price', $sortValue);
+                    });
+            });
+    }
+
+    public function scopeFilterBase($query, $request)
+    {
+        return $query
+            ->when($request->has('filter.category'), function ($query) use ($request) {
+                $list = explode(',', $request->query('filter')['category']);
+                $query->whereIn('category_name', $list);
+            })
+            ->when($request->has('filter.author'), function ($query) use ($request) {
+                $list = explode(',', $request->query('filter')['author']);
+                $query->whereIn('author_name', $list);
+            })
+            ->when($request->has('filter.rating_star'), function ($query) use ($request) {
+                $rating_star = $request->query('filter')['rating_star'];
+                $min = 0;
+                if ($rating_star > 1) {
+                    $min = $rating_star - 1 + 0.1;
+                }
+                $query
+                    ->joinSub($this->getAverageRatingStar(), 'book_avg_rating', function ($join) {
+                        $join->on('book.id', '=', 'book_avg_rating.id');
+                    })
+                    ->whereBetween('book_avg_rating.average_rating_star', [$min, $rating_star]);;
+            });
     }
 }
