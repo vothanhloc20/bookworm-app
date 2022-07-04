0c317ccff4f71467a34c44aea8a22b0899a62eba
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-07-04 21:17:08 +0700

    fix(fe): rewrite function in Home and Shop Adapter

--- resources/js/adapters/HomeAdapter/HomeAdapter.js
+++ resources/js/adapters/HomeAdapter/HomeAdapter.js
@@ -1,7 +1,6 @@
 import {
+    setFirstLoading,
     setIsRecommended,
-    setPopularBooks,
-    setRecommendedBooks,
     setTagFeaturedBooks,
     setTopTenOnSaleBooks,
 } from "../../redux/actions/home.action";
@@ -11,21 +10,32 @@ import bookApi from "../../api/bookApi";
 export const mapDispatchToProps = (dispatch) => {
     return {
         setTopTenOnSaleBooks: (data) => dispatch(setTopTenOnSaleBooks(data)),
-        setRecommendedBooks: (data) => dispatch(setRecommendedBooks(data)),
-        setPopularBooks: (data) => dispatch(setPopularBooks(data)),
         setTagFeaturedBooks: (data) => dispatch(setTagFeaturedBooks(data)),
         setIsRecommended: (data) => dispatch(setIsRecommended(data)),
+        setFirstLoading: (data) => dispatch(setFirstLoading(data)),
     };
 };
 
 export const getTopTenOnSaleBooks = () => {
-    return bookApi.getTopTenOnSaleBooks();
+    return bookApi.getBooks({
+        sortKey: "on_sale",
+        sortValue: "desc",
+        limit: 10,
+    });
 };
 
 export const getRecommendedBooks = () => {
-    return bookApi.getRecommendedBooks();
+    return bookApi.getBooks({
+        sortKey: "on_recommended",
+        sortValue: "desc",
+        limit: 8,
+    });
 };
 
 export const getPopularBooks = () => {
-    return bookApi.getPopularBooks();
+    return bookApi.getBooks({
+        sortKey: "on_popularity",
+        sortValue: "desc",
+        limit: 8,
+    });
 };
