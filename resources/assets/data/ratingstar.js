828b3e0c253a0adc8ca584f1c0af78f16bae03c8
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-07-06 19:16:15 +0700

    feat(fe): add new image source

--- resources/assets/data/ratingstar.js
+++ resources/assets/data/ratingstar.js
@@ -1,7 +1,1 @@
-export const ratingStar = [
-    { value: 1, label: "1" },
-    { value: 2, label: "2" },
-    { value: 3, label: "3" },
-    { value: 4, label: "4" },
-    { value: 5, label: "5" },
-];
+export const ratingStar = [1, 2, 3, 4, 5];
