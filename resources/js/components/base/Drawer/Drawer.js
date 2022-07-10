d3cc98ff7d6cb4fa1218ce4162de632ff81e28dd
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-07-10 10:38:23 +0700

    feat(fe): add logout function and UI for user when login successfully

--- resources/js/components/base/Drawer/Drawer.js
+++ resources/js/components/base/Drawer/Drawer.js
@@ -1,8 +1,12 @@
 import * as React from "react";
 
+import { Button } from "react-bootstrap";
 import Menu from "../Menu/Menu.js";
+import { Toast } from "../../../utils/toast.js";
+import UserInformation from "../UserInformation/UserInformation.js";
 import { closeDrawer } from "../../../redux/actions/drawer.action.js";
 import { connect } from "react-redux";
+import { logout } from "../../../adapters/AuthAdapter/AuthAdapter.js";
 import { mapStateToProps } from "../../../utils/useSelector.js";
 import { showModal } from "../../../redux/actions/modal.action.js";
 
@@ -19,6 +23,28 @@ class Drawer extends React.Component {
         this.props.closeDrawer();
     };
 
+    logout = async () => {
+        const token = localStorage.getItem("token");
+        logout(token);
+        if (result.message === "Logout Successfully") {
+            Toast.fire({
+                icon: "success",
+                title: "Logout successfully",
+                background: "#f0f9eb",
+                color: "#a5dc86",
+            });
+            localStorage.removeItem("token");
+            window.location.reload();
+        } else {
+            Toast.fire({
+                icon: "error",
+                title: "Logout unsuccessfully",
+                background: "#fde8e8",
+                color: "#f27474",
+            });
+        }
+    };
+
     render() {
         return (
             <>
@@ -31,13 +57,32 @@ class Drawer extends React.Component {
                     </div>
                 )}
                 <nav
-                    className={`app-nav-menu ${
+                    className={`app-nav-menu d-flex flex-column ${
                         this.props.drawer.status ? "amb-active" : "amb-inactive"
                     }`}
                 >
-                    <div className="d-flex flex-column p-4">
-                        <Menu handleModal={this.handleModal} />
+                    <div className="p-4 flex-grow-1">
+                        <div className="d-flex flex-column">
+                            <Menu handleModal={this.handleModal} />
+                        </div>
+                        {this.props.app.isLogin && (
+                            <div>
+                                <UserInformation />
+                            </div>
+                        )}
                     </div>
+                    {this.props.app.isLogin && (
+                        <div className="p-4">
+                            <Button
+                                variant="blue"
+                                block
+                                className="font-weight-bold"
+                                onClick={() => this.logout()}
+                            >
+                                Logout
+                            </Button>
+                        </div>
+                    )}
                 </nav>
             </>
         );
