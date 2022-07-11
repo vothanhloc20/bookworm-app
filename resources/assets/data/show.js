87fea62009c15075b0b57256d45340a5fde3e8a5
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-07-11 23:00:34 +0700

    fix(fe): Show Data (5, 15, 20, 25)

--- resources/assets/data/show.js
+++ resources/assets/data/show.js
@@ -3,12 +3,12 @@ export const showData = [
         title: "Show 5",
     },
     {
-        title: "Show 10",
-    },
-    {
         title: "Show 15",
     },
     {
         title: "Show 20",
+    },
+    {
+        title: "Show 25",
     },
 ];
