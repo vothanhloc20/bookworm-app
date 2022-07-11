56f8ed9036e9bb634f7b42f136441bae82eba5cc
Võ Thành Lộc <vothanhloc20@gmail.com>
2022-07-11 15:29:07 +0700

    feat(be): update routes\api.php

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,8 +20,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('user/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'userInformation']);
+    Route::resource('order', OrderController::class)->only(['store']);
+});
+
+Route::prefix('/auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
 
 Route::resource('authors', AuthorController::class)->only(['index']);
